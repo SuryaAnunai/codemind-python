7f907d14afff7c1f058fcbea1151c12e87bab74b
SuryaAnunai <109718235+SuryaAnunai@users.noreply.github.com>
2022-10-29 11:51:05 +0530

    def is_prime(num):     if num<2:         return False     for i in range(2,int(num*0.5)+1):         if num%i==0:             return False     return True def run_loop(n,x):     while is_prime(n)==False:         n+=x     return n n=int(input()) pp=run_loop(n,-1) np=run_loop(n,1) if(n-pp)<(np-n):     print(n-pp) elif (n-pp)==(np-n):     print(n-pp) else:     print(np-n)

---
+++ Minimum_absolute_difference_of_a_number_and_its_closest_prime.py
@@ -0,0 +1,21 @@
+def is_prime(num):
+    if num<2:
+        return False
+    for i in range(2,int(num*0.5)+1):
+        if num%i==0:
+            return False
+    return True
+def run_loop(n,x):
+    while is_prime(n)==False:
+        n+=x
+    return n
+n=int(input())
+pp=run_loop(n,-1)
+np=run_loop(n,1)
+if(n-pp)<(np-n):
+    print(n-pp)
+elif (n-pp)==(np-n):
+    print(n-pp)
+else:
+    print(np-n)
+                
