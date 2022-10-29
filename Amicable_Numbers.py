91309d4854fd3219d138151417e1d7b0e24e7078
SuryaAnunai <109718235+SuryaAnunai@users.noreply.github.com>
2022-10-29 09:53:43 +0530

    def func(num):     fs=0     for i in range(1,num):         if num%i==0:             fs+=i     return fs a=int(input()) b=int(input()) if(func(b)==a and func(a)==b):     print("Amicable") else:     print("Not Amicable")

---
+++ Amicable_Numbers.py
@@ -0,0 +1,12 @@
+def func(num):
+    fs=0
+    for i in range(1,num):
+        if num%i==0:
+            fs+=i
+    return fs
+a=int(input())
+b=int(input())
+if(func(b)==a and func(a)==b):
+    print("Amicable")
+else:
+    print("Not Amicable")
